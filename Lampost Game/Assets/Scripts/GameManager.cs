dbc43a6289e467d78c2358c419f0180cfbd2e9c7
KalvinMalloch <km230683@falmouth.ac.uk>
2019-10-19 12:18:53 +0100

    Actual Game

---
+++ Lampost Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject light1, light2, light3;
+    public bool IsLightChosen;
+    public int LightChosen;
+    public float Countdown = 5f;
+    public int nextLight;
+    float switchFlickerDelay = 0.2f;
+
+
+    void Start()
+    {
+        IsLightChosen = false;
+
+        nextLight = Random.Range(1, 4);
+
+        LightChosen = nextLight;
+    }
+
+    void Update()
+    {
+        RandomLight();
+        TurnLightsOn();
+        FlickerLight(nextLight);
+    }
+
+    void RandomLight() 
+    {
+        if (IsLightChosen == false) 
+        {
+            int previousLight = LightChosen;        //Code provided by Cole
+            while(nextLight == previousLight){        //Code provided by Cole
+                nextLight = Random.Range(1, 4);
+            }
+            IsLightChosen = true;
+            Countdown = 5f;
+        }
+
+        if (Countdown >= 0) 
+        {
+            Countdown = Countdown - (Time.deltaTime * 1);
+        }
+
+        if (Countdown <= 0) 
+        {
+            LightChosen = nextLight;
+            IsLightChosen = false;
+        }
+    }
+
+    void FlickerLight(int light)
+    {
+
+        if(switchFlickerDelay <= 0.8f)
+        {
+            if (light == 1)
+            {
+                light1.SetActive(false);
+            }
+            else if (light == 2)
+            {
+                light2.SetActive(false);
+            }
+            else if (light == 3)
+            {
+                light3.SetActive(false);
+            }
+        }
+        else if(switchFlickerDelay >= 0.9f)
+        {
+            if (light == 1)
+            {
+                light1.SetActive(true);
+            }
+            else if (light == 2)
+            {
+                light2.SetActive(true);
+            }
+            else if (light == 3)
+            {
+                light3.SetActive(true);
+            }
+        }
+
+        switchFlickerDelay -= Time.deltaTime;
+        if(switchFlickerDelay <= 0)
+        {
+            switchFlickerDelay = 1;
+        }
+    }
+
+    void TurnLightsOn() 
+    {
+        if (LightChosen == 1)
+        {
+            light1.SetActive (true);
+            light2.SetActive (false);
+            light3.SetActive (false);
+        }
+        else if (LightChosen == 2)
+        {
+            light1.SetActive (false);
+            light2.SetActive (true);
+            light3.SetActive (false);
+        }
+        else if (LightChosen == 3)
+        {
+            light1.SetActive (false);
+            light2.SetActive (false);
+            light3.SetActive (true);
+        }
+    }
+}
---
+++ Lampost Game/Assets/Scripts/MovementScript.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+	private Rigidbody2D rb2d;
+	public bool moveControls;
+	public bool grounded;
+    public bool inLight;
+	public float jumpPower = 250f;
+	public float speed = 1f;
+    public float depression = 100f;
+    public float depressionSpeed = 0.1f;
+	Animator anim;
+	Light glow;
+
+    void Awake () 
+    {
+		rb2d = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+		glow = GetComponent<Light> ();
+		moveControls = true;
+        inLight = false;
+		grounded = true;
+	}
+	
+    void Update() 
+    {
+        PlayerInLight();
+		LightDim();
+		anim.SetBool ("IsRunning", true);
+		float horizontal = Input.GetAxis ("Horizontal");
+        if (moveControls == true) {
+			Movement (horizontal);
+			if (Input.GetButtonDown("Jump") & (grounded == true)) {
+				anim.SetBool ("IsRunning", false);
+				rb2d.AddForce(Vector2.up * jumpPower);
+				grounded = false;
+			}
+		}
+    }
+	
+    void PlayerInLight()
+    {
+        if (inLight == false) 
+        {
+            depression = depression - (Time.deltaTime * depressionSpeed);
+            depressionSpeed = depressionSpeed + (Time.deltaTime * 0.5f);
+        }
+        if (inLight == true && depression <= 100)
+        {
+            depression = depression + (Time.deltaTime * depressionSpeed);
+        }
+        if (depression > 100)
+        {
+            depression = 100;
+        }
+    }
+	
+	void LightDim() 
+	{
+		glow.range = 5;
+	}
+	
+	private void Movement (float horizontal) 
+    {
+		rb2d.velocity = new Vector2(horizontal * speed, rb2d.velocity.y);
+		if (((Input.GetKey(KeyCode.A) == true) || (Input.GetKey(KeyCode.D) == true)) && (grounded == true)) {
+			anim.SetBool ("IsRunning", true);
+		} else if ((Input.GetKey(KeyCode.A) == false) || (Input.GetKey(KeyCode.D) == false)) {
+			anim.SetBool ("IsRunning", false);
+		}
+		if (Input.GetKey(KeyCode.A) == true) {
+			transform.localScale = new Vector3(-6, 6, 1);
+		}
+		if (Input.GetKey(KeyCode.D) == true) {
+			transform.localScale = new Vector3(6, 6, 1);
+		}
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D Lamp)
+    {
+        if (Lamp.tag == "Light")
+        {
+            inLight = true;
+        }
+		if (Lamp.tag == "Ground")
+        {
+            grounded = true;
+        }
+    }
+    void OnTriggerExit2D(Collider2D Lamp)
+    {
+        if (Lamp.tag == "Light")
+        {
+            inLight = false;
+        }
+    }
+}
