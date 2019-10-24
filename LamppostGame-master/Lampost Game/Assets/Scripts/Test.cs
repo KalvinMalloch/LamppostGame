b2305e3d403ad8cecdc0595085e3be6cc13e612d
Isaac <isaaclouiswright@gmail.com>
2019-10-24 23:42:23 +0100

    Retrying item update

---
+++ LamppostGame-master/Lampost Game/Assets/Scripts/Test.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D Player)
+    {
+        if (Player.tag == "Player")
+        {
+            MovementScript PlayerScripts = Player.GetComponent<MovementScript>();
+            PlayerScripts.depression = PlayerScripts.depression + 30;
+
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ LamppostGame-master/Lampost Game/Assets/SpawnItem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class SpawnItem : MonoBehaviour
+{
+    public GameObject item;
+    public float spawnTime = 2f;
+    public float fallSpeed = 40.0f; 
+    private float timer = 0; //counting timer, reset after calling SpawnRandom() function
+    private int randomNumber;       //variable for storage of an random Number
+
+    void Update()
+    {
+        timer += Time.deltaTime;   // Timer Counter
+        if (timer > spawnTime)
+        {
+            SpawnRandom();       //Calling method SpawnRandom()
+            timer = 0;        //Reseting timer to 0
+        }
+    }
+    public void SpawnRandom()
+    {
+
+        //Creating random Vector3 position
+        Vector3 screenPosition = (new Vector3(0, 13, 3));
+        Instantiate(item, screenPosition, Quaternion.identity);;
+    }
+}
